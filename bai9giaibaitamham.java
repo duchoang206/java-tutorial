116d92275c3077643de1a6610b4f44ded4a579c7
Hoàng Xuân Đức <hhoangduc2006@gmail.com>
2026-03-31 23:21:36 +0700

    Update bai 10

--- bai9giaibaitamham.java
+++ bai9giaibaitamham.java
@@ -10,32 +10,38 @@ public class bai9giaibaitamham {
     public static int Cong( int a , int b) {
         int kq1 = a +b;
         return kq1;
+        System.out.println(kq1);
     }
     public static int Tru (int a , int b ) {
         int kq2 = a -b;
         return kq2;
+        System.out.println(kq2);
     }
     public static int Nhan( int a , int b) {
         int kq3 = a*b;
         return kq3;
+        System.out.println(kq3);
     }
     public static double Chia( int a , int b ) {
-        if ( b==0)
+        if ( b==0) {
             System.out.println("khong chia duoc");
-        else {
+            return 0;
+        } else {
             double kq4 = a/b;
             return kq4;
+            System.out.println(kq4);
     }
 }
+
 public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    int a = sc.nextInt();
     System.out.println("Moi nhap a ");
-    int b = sc.nextInt();
+    int a = sc.nextInt();
     System.out.println(" Moi nhap b");
+    int b = sc.nextInt();
     sc.nextLine();
-    String phepTinh = sc.nextLine();
     System.out.println("Moi nhap + , - , / , * ");
+    String phepTinh = sc.nextLine();
     switch ( phepTinh) {
         case "+" :
             Cong(a,b);
@@ -52,6 +58,7 @@ public static void main(String[] args) {
         default:
             System.out.println("Khong thoa man");      
     }
-    sc.close();
+    
+
 }
 }
