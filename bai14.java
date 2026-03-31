86e537ea72cfee8255d0bdda51477dac3eb5965f
Hoàng Xuân Đức <hhoangduc2006@gmail.com>
2026-04-01 02:05:12 +0700

    bài 14 mảng

---
+++ bai14.java
@@ -0,0 +1,28 @@
+/*Cho một mảng các số nguyên n phần tử arr được nhập từ bàn phím. 
+Bạn hãy viết chương trình hiển thị ra màn hình những số chẵn trong mảng. Ví dụ nếu bạn nhập:
+
+5
+2 7 6 8 9
+Thì màn hình sẽ hiển thị:
+
+2 6 8 */
+
+import java.util.Scanner;
+
+public class bai14 {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int[] arr = new int[n];
+    for ( int i =0 ; i < n ; i++) {
+        arr[i] = sc.nextInt();
+        }
+    for ( int i = 0 ; i < n ; i++ ) {
+        if ( arr[i] %2 ==0 ) {
+            System.out.println(arr[i]);
+        }
+    }    
+    }
+    }
+    
+
