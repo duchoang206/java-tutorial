110028bc0260ab019c44d15b407822a39a6ccead
Hoàng Xuân Đức <hhoangduc2006@gmail.com>
2026-04-08 23:06:44 +0700

    Date trong java

---
+++ bai36.java
@@ -0,0 +1,16 @@
+public class bai36 {
+    public static void main(String[] args) {
+        System.out.println("pi =" + Math.PI);
+        //tri tuyet doi
+        int a =-8;
+        System.out.println("Tri tuyet doi cua a la "+ Math.abs(a));
+        System.out.println(Math.max(4,7));
+        System.out.println(Math.min(4,7));
+        System.out.println(Math.sqrt(4));
+        System.out.println(Math.pow(2,3));
+        int goc = 90;
+        double sin = Math.sin(Math.PI * goc /180 );
+        System.out.println(sin);
+
+    }
+}
