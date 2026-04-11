c0ac593db418528135c7e23a39d840ed29bd61b4
Hoàng Xuân Đức <hhoangduc2006@gmail.com>
2026-04-11 16:49:09 +0700

    compare to

---
+++ bai38.java
@@ -0,0 +1,14 @@
+/* kiểu char trong java , trong java khai báo nháy đơn bằng char còn nháy kép là cho chuỗi  */
+public class bai38 {
+    public static void main(String[] args) {
+        char ch = 'x';
+        char ch1 = 65;
+        char ch2 = 20;
+        // Compare to 
+        System.out.println(Character.compare(ch1, ch2));
+        System.out.println(Character.compare('x','y'));
+    }
+
+    
+    
+}
