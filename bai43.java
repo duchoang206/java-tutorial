3571c19d4a6584f10821b1f5401eabcab62e1a0f
Hoàng Xuân Đức <hhoangduc2006@gmail.com>
2026-04-13 02:01:21 +0700

    String va Array

--- bai43.java
+++ bai43.java
@@ -7,15 +7,20 @@ public class bai43 {
         String str1 ="English = 78 Science = 83 Math = 68 History = 65 ";
         int sum =0;
         int count =0;
-        for ( int i =0 ; i < str1.length() ; i++) {
-            char c = str1.charAt(i);
-            if ( Character.isDigit(c)) {
-                sum+= c;
-                count++;
-            }
+        String str2 = str1.replaceAll("[^0-9]+", " ").trim();
+        System.out.println(str2);
+        String[] mang = str2.split(" ");
+        for (String num : mang) {
+        try {
+            int num1 = Integer.parseInt(num);
+            sum += num1;
+            count++;
+        } catch (Exception e) {
+            // TODO: handle exception
         }
+    }
         System.out.println(sum);
         System.out.println(sum/count);
     }
-    
+
 }
