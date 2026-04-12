fc417acc34ef6fc9b73cfdd4f5722bfc1e3a455d
Hoàng Xuân Đức <hhoangduc2006@gmail.com>
2026-04-13 01:38:58 +0700

    Bai 43 String

--- bai43.java
+++ bai43.java
@@ -1,3 +1,21 @@
+/* giai bai tập java String 
+bài 19 String str1 ="English = 78 Science = 83 Math = 68 History = 65 "
+tính tổng các số trong chuỗi trên
+tính trung bình cộng */
 public class bai43 {
+    public static void main(String[] args) {
+        String str1 ="English = 78 Science = 83 Math = 68 History = 65 ";
+        int sum =0;
+        int count =0;
+        for ( int i =0 ; i < str1.length() ; i++) {
+            char c = str1.charAt(i);
+            if ( Character.isDigit(c)) {
+                sum+= c;
+                count++;
+            }
+        }
+        System.out.println(sum);
+        System.out.println(sum/count);
+    }
     
 }
