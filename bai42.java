f0b6e985de94ee1f1b017b352ba1aa05d9e88952
Hoàng Xuân Đức <hhoangduc2006@gmail.com>
2026-04-12 20:56:35 +0700

    van dung String

--- bai42.java
+++ bai42.java
@@ -7,13 +7,35 @@
 import java.util.Scanner;
 
 public class bai42 {
+    public static void main(String[] args) { 
     Scanner sc = new Scanner(System.in);
+    System.out.println("nhap vao chuoi~");
     String s = sc.nextLine();
     // khởi tạo biến đếm 
     int thuong = 0;
     int inhoa =0;
-    int khoangtrang =0;
     int so =0;
+    int khoangtrang =0;
+    for ( int i = 0 ; i < s.length() ; i++) {
+        char c = s.charAt(i);
+        if ( Character.isLowerCase(c)) {
+            thuong++;
+        }
+        if ( Character.isUpperCase(c)) {
+            inhoa++;
+        }
+        if ( Character.isDigit(c)) {
+            so++;
+        }
+        if ( Character.isWhitespace(c)) {
+            khoangtrang++;
+        }
+    }
+    System.out.println(thuong);
+    System.out.println(so);
+    System.out.println(khoangtrang);
+    System.out.println(inhoa);
+
     
-    
+}
 }
