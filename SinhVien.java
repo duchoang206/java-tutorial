9f1a01d1275b82f96c83554689fefb53bf5da3c6
Hoàng Xuân Đức <hhoangduc2006@gmail.com>
2026-04-14 03:26:40 +0700

    oop java

---
+++ SinhVien.java
@@ -0,0 +1,10 @@
+public class SinhVien {
+    // Thuộc tính
+    private String hoTen;
+    private double diem;
+
+    // Phương thức
+    public void hienthithongtin() {
+        System.out.println(hoTen + diem);
+    }
+}
---
+++ bai61oop.java
@@ -0,0 +1,15 @@
+/* 
+hướng đối tượng trong java 
+đối tượng object trong lập trình như 1 đối tượng cụ thể trong thế giới thực mỗi đối tượng có thuộc tính và hành vi riêng 
+thuộc tính là đặc điểm của đối tượng
+phương thức là hành vi của  đối tượng
+các đối tượng có phương thức thuộc tính giống nhau được gom thành 1 lớp để dễ quản lý 
+ */
+public class bai61oop {
+    public static void main(String[] args) {
+        //  khởi tạo đối tượng thuộc class sinh viên 
+        SinhVien sv1 =new SinhVien();
+        sv1.hienthithongtin();
+    }
+    
+}
